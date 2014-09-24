a01c53aee4d913b6e1106009f111ebcdd358e3dc
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-24 14:26:28 +0800

    sync daily version 2.0.3

---
+++ 2.0.3/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-08-12 2:25:03 PM*/
+KISSY.config("modules",{"kg/mobile-tab/2.0.1/mini":{requires:["node","base"]}});
---
+++ 2.0.3/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/mobile-tab/2.0.1/mini': { requires: ['node', 'base']} 
+});
